8b1ec3906cf4502b3222db3b99f9b2d0bb7624e4
jake77895 <jspitzer@chicagobooth.edu>
2024-11-19 17:30:56 +0000

    Added foreign keys

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,10 +2,12 @@
 #
 # Table name: boards
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
+#  integer    :string
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :string
 #
 class Board < ApplicationRecord
   validates(:name, presence: true, uniqueness: true)
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,15 @@
 #
 # Table name: posts
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
 #  body       :text
 #  expires_on :date
+#  integers   :string
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  board_id   :integer
+#  user_id    :string
 #
 class Post < ApplicationRecord
   validates(:title, presence: true)
